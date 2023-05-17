d48fb3bb1c236cf904b1e403bf0509196bfa55b7
Austen Stone <austenstone@github.com>
2023-05-17 15:23:10 -0400

    test

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
+import { Octokit } from "octokit";
 
-console.log('hi');
+export interface WorkflowDispatchParams {
+    token: string;
+    owner: string;
+    repo: string;
+    workflow_id: string;
+    ref: string;
+    inputs?: any;
+}
+
+export const workflowDispatchSync = async (params: WorkflowDispatchParams) => {
+    const octokit = new Octokit({ auth: params.token });
+    try {
+        const response = await octokit.rest.actions.createWorkflowDispatch({
+            ...params
+        });
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,5 +1,24 @@
 import { test, expect } from '@jest/globals';
+import { workflowDispatchSync } from '../src/index';
 
-test('Test 1', () => {
+if (!process.env.GITHUB_TOKEN) throw new Error('GITHUB_TOKEN not set in environment variables!');
+
+const token = process.env.GITHUB_TOKEN;
+const ownerRepo = {
+  owner: 'austenstone',
+  repo: 'workflow-dispatch-sync'
+}
+const ref = 'main';
+const workflow_id = 'test_1_basic.yml';
+
+test('Basic Dispatch', async () => {
+  console.log({ token, ...ownerRepo, workflow_id, ref })
+  console.log(`Dispatching ${ownerRepo.owner}/${ownerRepo.repo}/.github/workflows/${workflow_id}@${ref}`);
+  await workflowDispatchSync({
+    token,
+    ...ownerRepo,
+    workflow_id,
+    ref
+  });
   expect(true).toBe(true);
 });
