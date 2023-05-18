d9734494269a801fb613fb9c6dfe503095bf6455
Austen Stone <austenstone@github.com>
2023-05-18 13:38:21 -0400

    init

--- src/index.ts
+++ src/index.ts
@@ -1,121 +1,120 @@
-import { Octokit } from "octokit";
 import { Endpoints } from "@octokit/types";
 
-import { createServer } from "node:http";
+import { Server, createServer } from "node:http";
 import { App, createNodeMiddleware } from "octokit";
+import { EmitterWebhookEvent } from "@octokit/webhooks/dist-types/index";
 
-import EventSource from "eventsource";
 const SmeeClient = require('smee-client')
+import EventSource from "eventsource";
 
 import { v4 as uuidv4 } from 'uuid';
 
 import * as dotenv from "dotenv";
 dotenv.config()
+export class WorkflowDispatch {
+  app: App;
+  private pendingDispatches: {
+    uid: string;
+    requested?: (run: EmitterWebhookEvent<'workflow_run.requested'>['payload']) => void;
+    progress?: (run: EmitterWebhookEvent<'workflow_run.in_progress'>['payload']) => void;
+    resolve: (run: EmitterWebhookEvent<'workflow_run.completed'>['payload']) => void;
+    reject: (reason?: any) => void;
+  }[] = [];
+  private source: EventSource;
+  private port: number;
+  private events: any;
+  private smee?: any;
+  private server: Server;
 
-if (!process.env.GITHUB_TOKEN) throw new Error("GITHUB_TOKEN not set in environment variables!");
-if (!process.env.APP_ID) throw new Error("APP_ID not set in environment variables!");
-if (!process.env.PRIVATE_KEY) throw new Error("PRIVATE_KEY not set in environment variables!");
-if (!process.env.WEBHOOK_SECRET) throw new Error("WEBHOOK_SECRET not set in environment variables!");
-if (!process.env.CLIENT_ID) throw new Error("CLIENT_ID not set in environment variables!");
-if (!process.env.CLIENT_SECRET) throw new Error("CLIENT_SECRET not set in environment variables!");
+  constructor(app: App, port = 3000, webhookProxyUrl = process.env.WEBHOOK_PROXY_URL) {
+    this.app = app;
+    this.port = port;
 
-const port = 3000;
-
-const webhookProxyUrl = process.env.WEBHOOK_PROXY_URL; // replace with your own Webhook Proxy URL
-
-const token = process.env.GITHUB_TOKEN;
-const ownerRepo = {
-  owner: 'austenstone',
-  repo: 'workflow-dispatch-sync'
-}
-const ref = 'main';
-const workflow_id = 'test_1_basic.yml';
-
-const app = new App({
-  appId: process.env.APP_ID,
-  privateKey: process.env.PRIVATE_KEY,
-  webhooks: {
-    path: "/",
-    secret: process.env.WEBHOOK_SECRET
-  },
-  oauth: {
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-  }
-});
-
-app.webhooks.onAny((event) => {
-  console.log("Webhook event received:", event.name);
-});
-
-app.webhooks.on("workflow_run.completed", async ({ octokit, payload }) => {
-  console.log("workflow_run.completed", payload.workflow_run.jobs_url);
-
-  const jobsResponse = await octokit.request(payload.workflow_run.jobs_url);
-  jobsResponse.data.jobs.forEach((job) => {
-    job.steps.forEach((step) => {
-      console.log(`Step ${step.name} ${step.conclusion}`)
-      const dispatch = pendingDispatches.find((dispatch) => dispatch.uid === step.name);
-      if (dispatch) {
-        console.log(`Dispatch ${dispatch.uid} ${step.conclusion}`)
-        step.conclusion === 'success' ? dispatch.resolve(payload) : dispatch.reject(payload);
-      }
-    });
-  });
-});
-
-const smee = new SmeeClient({
-  source: webhookProxyUrl,
-  target: `http://localhost:${port}/`,
-  logger: console
-})
-
-const source = new EventSource(webhookProxyUrl);
-source.onmessage = (event) => {
-  const webhookEvent = JSON.parse(event.data);
-  app.webhooks
-    .verifyAndReceive({
-      id: webhookEvent["x-request-id"],
-      name: webhookEvent["x-github-event"],
-      signature: webhookEvent["x-hub-signature"],
-      payload: webhookEvent.body,
-    })
-    .catch(console.error);
-};
-
-const pendingDispatches: any[] = [];
-
-type WorkflowDispatchParameters = Endpoints["POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches"]["parameters"];
-export const workflowDispatchSync = async (token: string, parameters: WorkflowDispatchParameters) => {
-  const octokit = new Octokit({ auth: token });
-  if (!parameters.inputs) parameters.inputs = {};
-  if (!parameters.inputs.uid) parameters.inputs.uid = uuidv4();
-  await octokit.rest.actions.createWorkflowDispatch(parameters);
-  return new Promise((resolve, reject) => {
-    pendingDispatches.push({
-      uid: parameters.inputs?.uid,
-      resolve,
-      reject
-    });
-  });
-}
-
-console.log(`Dispatching ${ownerRepo.owner}/${ownerRepo.repo}/.github/workflows/${workflow_id}@${ref}`);
-
-(async () => {
-  const uid = uuidv4();
-  await workflowDispatchSync(token, {
-    ...ownerRepo,
-    workflow_id,
-    ref,
-    inputs: {
-      uid
+    if (webhookProxyUrl) {
+      console.log(`Setting up Smee with ${webhookProxyUrl}`)
+      this.setupSmee(webhookProxyUrl);
     }
-  });
-})();
+    
+    this.app.webhooks.on("workflow_run", this.onWorkflowRun);
+    
+    createServer(createNodeMiddleware(app)).listen(port);
+  }
 
-const events = smee.start()
+  private setupSmee = (webhookProxyUrl: string) => {
+    this.smee = new SmeeClient({
+      source: webhookProxyUrl,
+      target: `http://localhost:${this.port}/`,
+      logger: console
+    })
 
-createServer(createNodeMiddleware(app)).listen(port);
+    this.source = new EventSource(webhookProxyUrl);
+    this.source.onmessage = (event) => {
+      const webhookEvent = JSON.parse(event.data);
+      this.app.webhooks
+        .verifyAndReceive({
+          id: webhookEvent["x-request-id"],
+          name: webhookEvent["x-github-event"],
+          signature: webhookEvent["x-hub-signature"],
+          payload: webhookEvent.body,
+        })
+        .catch(console.error);
+    };
 
-events.close()
+    this.events = this.smee.start()
+  };
+
+  private onWorkflowRun = async ({ octokit, payload }) => {
+    const jobsResponse = await octokit.request(payload.workflow_run.jobs_url);
+    jobsResponse.data.jobs.forEach((job) => {
+      job.steps.forEach((step) => {
+        const dispatch = this.pendingDispatches.find((dispatch) => dispatch.uid === step.name);
+        if (dispatch) {
+          switch (payload.action) {
+            case 'completed':
+              dispatch.resolve(payload);
+              break;
+            case 'requested':
+              if (dispatch.requested) dispatch.requested(payload);
+              break;
+            case 'in_progress':
+              if (dispatch.progress) dispatch.progress(payload);
+              break;
+          }
+        }
+      });
+    });
+  }
+
+  workflowDispatchSync = async (
+    parameters: Endpoints["POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches"]["parameters"],
+    requested?: (run: EmitterWebhookEvent<'workflow_run.requested'>['payload']) => void,
+    progress?: (run: EmitterWebhookEvent<'workflow_run.in_progress'>['payload']) => void
+  ) => {
+    if (!parameters.inputs) parameters.inputs = {};
+    if (!parameters.inputs.uid) parameters.inputs.uid = uuidv4();
+    for await (const { octokit, repository } of this.app.eachRepository.iterator()) {
+      if (repository.name !== parameters.repo) continue;
+      console.log('found repo', repository.name)
+      break;
+      await octokit.rest.actions.createWorkflowDispatch(parameters);
+    }
+    return new Promise<EmitterWebhookEvent<'workflow_run.completed'>['payload']>((resolve, reject) => {
+      this.pendingDispatches.push({
+        uid: parameters.inputs?.uid as string,
+        requested,
+        progress,
+        resolve,
+        reject
+      });
+    }).finally(() => {
+      const index = this.pendingDispatches.findIndex((dispatch) => dispatch.uid === parameters.inputs?.uid);
+      if (index >= 0) this.pendingDispatches.splice(index, 1);
+    });
+  }
+
+  close = () => {
+    this.source.close();
+    this.events.close();
+  }
+
+}
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,22 +1,41 @@
 import { test, expect } from '@jest/globals';
-import { workflowDispatchSync } from '../src/index';
+import { WorkflowDispatch } from '../src/index';
+import { App } from 'octokit';
 
 if (!process.env.GITHUB_TOKEN) throw new Error('GITHUB_TOKEN not set in environment variables!');
 
-const token = process.env.GITHUB_TOKEN;
-const ownerRepo = {
-  owner: 'austenstone',
-  repo: 'workflow-dispatch-sync'
-}
-const ref = 'main';
-const workflow_id = 'test_1_basic.yml';
+jest.setTimeout(30000);
 
 test('Basic Dispatch', async () => {
-  console.log(`Dispatching ${ownerRepo.owner}/${ownerRepo.repo}/.github/workflows/${workflow_id}@${ref}`);
-  await workflowDispatchSync(token, {
-    ...ownerRepo,
-    workflow_id,
-    ref
-  });
-  expect(true).toBe(true);
+  if (!process.env.GITHUB_TOKEN) throw new Error("GITHUB_TOKEN not set in environment variables!");
+  if (!process.env.APP_ID) throw new Error("APP_ID not set in environment variables!");
+  if (!process.env.PRIVATE_KEY) throw new Error("PRIVATE_KEY not set in environment variables!");
+  if (!process.env.WEBHOOK_SECRET) throw new Error("WEBHOOK_SECRET not set in environment variables!");
+  if (!process.env.CLIENT_ID) throw new Error("CLIENT_ID not set in environment variables!");
+  if (!process.env.CLIENT_SECRET) throw new Error("CLIENT_SECRET not set in environment variables!");
+
+  const dispatcher = new WorkflowDispatch(new App({
+    appId: process.env.APP_ID,
+    privateKey: process.env.PRIVATE_KEY,
+    webhooks: {
+      path: "/",
+      secret: process.env.WEBHOOK_SECRET
+    },
+    oauth: {
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET
+    }
+  }));
+  const params = {
+    owner: 'austenstone',
+    repo: 'workflow-dispatch-sync',
+    ref: 'main',
+    workflow_id: 'test_1_basic.yml'
+  };
+  const run = await dispatcher.workflowDispatchSync(params);
+  expect(run.workflow_run.status).toBe('completed');
+  expect(run.workflow_run.conclusion).toBe('success');
+  expect(run.workflow_run.head_branch).toBe(params.ref);
+  expect(run.workflow_run.head_sha).toBe(run.workflow_run.head_commit.id);
+  expect(run.workflow_run.event).toBe('workflow_dispatch');
 });
